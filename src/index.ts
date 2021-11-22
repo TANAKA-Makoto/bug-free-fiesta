1e3c653b118b3bb609839dcb0d040cd29343f4bd
TANAKA-Makoto <45248369+TANAKA-Makoto@users.noreply.github.com>
2021-11-22 17:17:01 +0900

    スキーマ自動読み込み

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Ajv, { JSONSchemaType } from "ajv";
 import * as ApiModels from "./@type/OAStype/model/models";
+import { glob } from "glob";
 
 import * as fs from "fs";
 import { exit } from "process";
@@ -10,42 +12,32 @@ type ApiRobotSpecification =
   | ApiModels.ElevatorSpecification
   | ApiModels.NavigationSpecification;
 
-const schema_owner: unknown = JSON.parse(
-  fs.readFileSync("raas-front-OAS/models/ownerParams.v1.json", "utf8")
-);
-const schema_robot: unknown = JSON.parse(
-  fs.readFileSync("raas-front-OAS/models/robotParams.v1.json", "utf8")
-);
-const schema_wp: unknown = JSON.parse(
-  fs.readFileSync("raas-front-OAS/models/waypointParams.v2.json", "utf8")
-);
-const schema_id: unknown = JSON.parse(
-  fs.readFileSync("raas-front-OAS/models/common/idObject.v1.json", "utf8")
-);
-const ownerParam: JSONSchemaType<ApiModels.OwnerParams> = schema_owner;
-//const robotParam: JSONSchemaType<ApiRobotParams> = schema_robot;
-const wpParam: JSONSchemaType<ApiModels.WaypointParams> = schema_wp;
-const idST: JSONSchemaType<{ id: string }> = schema_id;
 // validate is a type guard for MyData - type is inferred from schema type
 //const validate = ajv.compile(ownerParam);
-ajv.addSchema(ownerParam, "owner");
-//ajv.addSchema(robotParam, "robot");
-ajv.addSchema(idST, "common/idObject.v1.json");
-ajv.addSchema(wpParam, "wp");
+
+const filePaths: string[] = glob.sync("raas-front-OAS/models/**/*.json");
+const addSchemas = (ajv: Ajv, filePath: string): void => {
+  const schema = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  const id: string = filePath.replace(/raas\-front\-OAS\/models\//, "");
+  console.log(id);
+  ajv.addSchema(schema, id);
+};
+filePaths.forEach((_) => addSchemas(ajv, _));
+//addSchemas(ajv, "raas-front-OAS/models/common/idObject.v1.json");
 
 // or, if you did not use type annotation for the schema,
 // type parameter can be used to make it type guard:
 // const validate = ajv.compile<MyData>(schema)
-const validate = ajv.getSchema<ApiModels.OwnerParams>("owner");
+const validate = ajv.getSchema<ApiModels.OwnerParams>("ownerParams.v1.json");
 //const robo_validate = ajv.getSchema<ApiRobotParams>("robot");
-const wp_validate = ajv.getSchema<ApiModels.WaypointParams>("wp");
+//const wp_validate = ajv.getSchema<ApiModels.WaypointParams>("wp");
 const data = {
   name: "hoge",
   owned_robot: ["abc"],
   owned_map: [""],
 };
 if (!validate) {
-  exit();
+  exit(2);
 }
 // if (!robo_validate) {
 //   exit();
