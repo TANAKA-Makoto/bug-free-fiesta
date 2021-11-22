9cbc7628b1ef968b090285abf63200f0b8a6629b
TANAKA-Makoto <45248369+TANAKA-Makoto@users.noreply.github.com>
2021-11-22 15:09:09 +0900

    よくわからん感じになってる

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,60 @@
 import Ajv, { JSONSchemaType } from "ajv";
+import * as ApiModels from "./@type/OAStype/model/models";
+
 import * as fs from "fs";
-const ajv = new Ajv();
+import { exit } from "process";
+export const ajv = new Ajv();
 
-interface MyData {
-  foo: number;
-  bar?: string;
-}
+type ApiRobotParams = ApiModels.ElevatorParams | ApiModels.NavigationParams;
+type ApiRobotSpecification =
+  | ApiModels.ElevatorSpecification
+  | ApiModels.NavigationSpecification;
 
-const schema: JSONSchemaType<MyData> = {
-  type: "object",
-  properties: {
-    foo: { type: "integer" },
-    bar: { type: "string", nullable: true },
-  },
-  required: ["foo"],
-  additionalProperties: false,
-};
-const ownerParam: JSONSchemaType<MyData> = fs.readFileSync(
-  "raas-front-OAS/models/ownerParams.v1.json"
+const schema_owner: unknown = JSON.parse(
+  fs.readFileSync("raas-front-OAS/models/ownerParams.v1.json", "utf8")
 );
+const schema_robot: unknown = JSON.parse(
+  fs.readFileSync("raas-front-OAS/models/robotParams.v1.json", "utf8")
+);
+const schema_wp: unknown = JSON.parse(
+  fs.readFileSync("raas-front-OAS/models/waypointParams.v2.json", "utf8")
+);
+
+const ownerParam: JSONSchemaType<ApiModels.OwnerParams> = schema_owner;
+//const robotParam: JSONSchemaType<ApiRobotParams> = schema_robot;
+const wpParam: JSONSchemaType<ApiModels.WaypointParams> = schema_wp;
 // validate is a type guard for MyData - type is inferred from schema type
-const validate = ajv.compile(ownerParam);
+//const validate = ajv.compile(ownerParam);
+ajv.addSchema(ownerParam, "owner");
+//ajv.addSchema(robotParam, "robot");
+ajv.addSchema(wpParam, "wp");
 
 // or, if you did not use type annotation for the schema,
 // type parameter can be used to make it type guard:
 // const validate = ajv.compile<MyData>(schema)
-
+const validate = ajv.getSchema<ApiModels.OwnerParams>("owner");
+//const robo_validate = ajv.getSchema<ApiRobotParams>("robot");
+const wp_validate = ajv.getSchema<ApiModels.WaypointParams>("wp");
 const data = {
-  foo: 1,
-  bar: "abc",
+  name: "hoge",
+  owned_robot: ["abc"],
+  owned_map: [""],
 };
-
+if (!validate) {
+  exit();
+}
+// if (!robo_validate) {
+//   exit();
+// }
 if (validate(data)) {
   // data is MyData here
-  console.log(data.foo);
+  console.log(data.name);
 } else {
   console.log(validate.errors);
 }
+// if (robo_validate(data)) {
+//   // data is MyData here
+//   console.log(data.name);
+// } else {
+//   console.log(robo_validate.errors);
+// }
